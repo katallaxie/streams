fce46f5054dd78bbbf9af31e8b5569087b746f26
Sebastian Döll <sebastian@katallaxie.me>
2023-01-02 20:00:51 +0000

    wip: basic metrics

--- metrics.go
+++ metrics.go
@@ -15,12 +15,12 @@ var (
 	DefaultGatherer   prometheus.Gatherer   = DefaultRegistry
 )
 
-// Registry ...
+// Registry is a prometheus registry.
 type Registry struct {
 	*prometheus.Registry
 }
 
-// NewRegistry ...
+// NewRegistry is a constructor for Registry.
 func NewRegistry() *Registry {
 	r := prometheus.NewRegistry()
 
@@ -38,41 +38,68 @@ func (r *Registry) Handler() http.Handler {
 	return promhttp.InstrumentMetricHandler(r, promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 }
 
-// DefaultMetrics ...
+// DefaultMetrics is a set of default metrics.
 var DefaultMetrics = NewMetrics()
 
-// Metrics ...
-type Metrics struct{}
+// Metrics is a set of metrics.
+type Metrics struct {
+	latency *prometheus.GaugeVec
+	count   *prometheus.CounterVec
+}
 
-// NewMetrics ...
+// NewMetrics is a constructor for Metrics.
 func NewMetrics() *Metrics {
 	m := new(Metrics)
+
+	m.latency = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "streams_latency",
+			Help: "Time between sourcing and sinking a message.",
+		},
+		[]string{
+			"streams_node",
+		},
+	)
+
+	m.count = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "streams_count",
+			Help: "Number of messages processed.",
+		},
+		[]string{
+			"streams_node",
+		},
+	)
 
 	return m
 }
 
-// Collect ...
+// Collect implements prometheus.Collector.
 func (m *Metrics) Collect(ch chan<- prometheus.Metric) {
+	m.latency.Collect(ch)
+	m.count.Collect(ch)
 }
 
-// Describe ...
+// Describe implements prometheus.Collector.
 func (m *Metrics) Describe(ch chan<- *prometheus.Desc) {
+	m.latency.Describe(ch)
+	m.count.Describe(ch)
 }
 
-// Monitor ...
+// Monitor is a statistics monitor.
 type Monitor struct {
 	metrics *Metrics
 
 	sync.Mutex
 }
 
-// Gatherer ...
+// Gatherer is a type that can gather metrics.
 type Gatherer interface {
 	// Gather ...
 	Gather(collector Collector)
 }
 
-// NewMonitor ...
+// NewMonitor is a constructor for Monitor.
 func NewMonitor(metrics *Metrics) *Monitor {
 	m := new(Monitor)
 	m.metrics = metrics
@@ -80,7 +107,7 @@ func NewMonitor(metrics *Metrics) *Monitor {
 	return m
 }
 
-// Gather ...
+// Gather is a method that gathers metrics.
 func (m *Monitor) Gather(collector Collector) {
 	m.Lock()
 	defer m.Unlock()
@@ -95,4 +122,14 @@ func (m *Monitor) Gather(collector Collector) {
 	}()
 
 	collector.Collect(ch)
+}
+
+// SetLatency sets the latency metric.
+func (m *Monitor) SetLatency(node string, latency float64) {
+	m.metrics.latency.WithLabelValues(node).Set(latency)
+}
+
+// SetCount sets the count metric.
+func (m *Monitor) SetCount(node string, count float64) {
+	m.metrics.count.WithLabelValues(node).Add(count)
 }
