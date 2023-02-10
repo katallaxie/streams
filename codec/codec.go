7f781adf2914575ac525301b766b83bccf821ed4
Sebastian Döll <sebastian@katallaxie.me>
2023-02-10 21:28:34 +0000

    wip: add int64 encoder/decoder

--- codec/codec.go
+++ codec/codec.go
@@ -1,5 +1,7 @@
 package codec
 
+import "strconv"
+
 // NilDecoder is a decoder that always returns a nil, no matter the input.
 var NilDecoder Decoder[any] = func([]byte) (any, error) { return nil, nil }
 
@@ -21,6 +23,11 @@ var ByteDecoder Decoder[[]byte] = func(b []byte) ([]byte, error) {
 	return b, nil
 }
 
+// Int64Decoder represents an int64 decoder.
+var Int64Decoder Decoder[int64] = func(b []byte) (int64, error) {
+	return strconv.ParseInt(string(b), 10, 64)
+}
+
 // Encoder is an adapter allowing to use a function as an encoder.
 type Encoder[T any] func(T) ([]byte, error)
 
@@ -37,4 +44,9 @@ var StringEncoder Encoder[string] = func(v string) ([]byte, error) {
 // ByteEncoder represents a byte encoder.
 var ByteEncoder Encoder[[]byte] = func(v []byte) ([]byte, error) {
 	return v, nil
+}
+
+// Int64Encoder represents an int64 encoder.
+var Int64Encoder Encoder[int64] = func(v int64) ([]byte, error) {
+	return []byte(strconv.FormatInt(v, 10)), nil
 }
