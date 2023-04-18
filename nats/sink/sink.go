e5c84a7abf0cf14b7816b631dc4735cecfa640db
Sebastian Döll <sebastian@katallaxie.me>
2023-04-18 18:22:55 +0000

    chore: add nats.io sink

--- nats/sink/sink.go
+++ nats/sink/sink.go
@@ -6,59 +6,50 @@ import (
 	"github.com/katallaxie/streams"
 	"github.com/katallaxie/streams/codec"
 	"github.com/katallaxie/streams/msg"
-
-	kgo "github.com/segmentio/kafka-go"
+	"github.com/nats-io/nats.go"
 )
 
 var _ streams.Sink[any, any] = (*Sink[any, any])(nil)
 
-// Sink is a Kafka sink.
+// Sink is a nats sink.
 type Sink[K, V any] struct {
-	writer       *kgo.Writer
+	conn         *nats.Conn
 	ctx          context.Context
 	keyEncoder   codec.Encoder[K]
 	valueEncoder codec.Encoder[V]
 }
 
-// WithContext is a constructor for a Kafka sink with a cancellation context.
-func WithContext[K, V any](ctx context.Context, w *kgo.Writer, key codec.Encoder[K], value codec.Encoder[V]) *Sink[K, V] {
+// WithContext is a constructor for a kafka sink with a cancellation context.
+func WithContext[K, V any](ctx context.Context, conn *nats.Conn, key codec.Encoder[K], value codec.Encoder[V]) *Sink[K, V] {
 	k := new(Sink[K, V])
 	k.ctx = ctx
-	k.writer = w
+	k.conn = conn
 	k.keyEncoder = key
 	k.valueEncoder = value
 
 	return k
 }
 
-// NewSink is a Kafka sink constructor.
+// NewSink is a nats sink constructor.
 func NewSink[K, V any]() *Sink[K, V] {
 	n := new(Sink[K, V])
 
 	return n
 }
 
-// Write is a Kafka sink writer.
+// Write is a nats sink writer.
 func (s *Sink[K, V]) Write(messages ...msg.Message[K, V]) error {
-	mm := make([]kgo.Message, len(messages))
-
-	for i, m := range messages {
+	for _, m := range messages {
 		val, err := s.valueEncoder.Encode(m.Value())
 		if err != nil {
 			return err
 		}
 
-		key, err := s.keyEncoder.Encode(m.Key())
+		err = s.conn.Publish(m.Topic(), val)
 		if err != nil {
 			return err
 		}
-
-		mm[i] = kgo.Message{
-			Key:   key,
-			Value: val,
-			Topic: m.Topic(),
-		}
 	}
 
-	return s.writer.WriteMessages(s.ctx, mm...)
+	return nil
 }
