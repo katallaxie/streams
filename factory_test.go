74066a49bbb66e2aa457208cab4e4520c18e4a3d
Sebastian Döll <sebastian@katallaxie.me>
2023-04-24 15:09:46 +0000

    chore: test count metric

--- factory_test.go
+++ factory_test.go
@@ -12,3 +12,21 @@ func TestNewStream(t *testing.T) {
 
 	assert.NotNil(t, s)
 }
+
+func TestCountMetric(t *testing.T) {
+	m := newCountMetric("foo")
+	m.inc()
+
+	assert.Equal(t, m.value, float64(1))
+	assert.Equal(t, m.nodeName, "foo")
+
+	ch := make(chan Metric)
+
+	go func() {
+		m.Collect(ch)
+	}()
+
+	mm := <-ch
+	assert.Equal(t, mm, m)
+	assert.Equal(t, m.value, float64(1))
+}
